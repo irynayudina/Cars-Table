6920fec4dccc1a115a4d20f4b4021865bb9fbecc
Iryna Yudina <yudinaira4444@gmail.com>
2023-06-30 21:27:22 +0300

    + loading component

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from 'react'
 import "./App.css";
 import Table from './components/table/Table';
 import SearchBar from './components/search/SearchBar';
+import Loading from "./components/loading/Loading";
 import loadCars from './utils/loadCars';
 
 function App() {
   const [cars, setCars] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
 
   useEffect(() => {
@@ -14,17 +16,29 @@ function App() {
     });
   }, []);
 
+  useEffect(() => {
+    if (cars) {
+      setIsLoading(false);
+    }
+  }, [cars]);
+
   
   return (
     <>
       <h1>Cars</h1>
-      <div className="card">
-        <SearchBar />
-      </div>
-      <div className="card">
-        <Table cars={cars} />
-      </div>
-      <p className="read-the-docs">Test task by Iryna Yudina</p>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <div className="card">
+            <SearchBar />
+          </div>
+          <div className="card">
+            <Table cars={cars} />
+          </div>
+        </>
+      )}
+      <p className="read-the-docs">Test task completed by Iryna Yudina</p>
       <p className="read-the-docs">yudinaira4444@gmail.com</p>
     </>
   );
--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,62 +1,62 @@
-import React, { useEffect } from 'react'
-import './Table.css'
-import { useState } from 'react';
+import React, { useState, useEffect } from "react";
+import "./Table.css";
+
 const Table = ({ cars }) => {
-    const portionSize = 10;
-    const [pageNum, setPageNum] = useState(10)
-    const [displayCars, setDisplayCars] = useState([]);
+  const portionSize = 10;
+  const [pageNum, setPageNum] = useState(10);
+  const [displayCars, setDisplayCars] = useState([]);
 
-    const loadMore = () => {
-        const carsPortion = cars.slice(pageNum, pageNum + portionSize);
-        setDisplayCars([...displayCars, ...carsPortion]);
-        setPageNum((prev) => prev + portionSize);
-    }
-    
-    useEffect(() => {
-        setDisplayCars(cars.slice(0, 0 + portionSize));
-    }, [cars])
-    
-  return (
-    <div className="table-container">
-      <table>
-        <thead>
-          <tr>
-            <th>Company</th>
-            <th>Model</th>
-            <th>VIN</th>
-            <th>Color</th>
-            <th>Year</th>
-            <th>Price</th>
-            <th>Availability</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {displayCars?.map((car) => (
-            <tr key={car.id}>
-              <td>{car.car}</td>
-              <td>{car.car_model}</td>
-              <td>{car.car_vin}</td>
-              <td className="color-car">
-                <div
-                  className="color-cirle"
-                  style={{ backgroundColor: `${car.car_color}` }}
-                ></div>
-                {car.car_color}
-              </td>
-              <td>{car.car_model_year}</td>
-              <td>{car.price}</td>
-              <td>{car.availability.toString()}</td>
-              <td>-Edit -Delete</td>
+  const loadMore = () => {
+    const carsPortion = cars.slice(pageNum, pageNum + portionSize);
+    setDisplayCars([...displayCars, ...carsPortion]);
+    setPageNum((prev) => prev + portionSize);
+  };
+
+  useEffect(() => {
+    setDisplayCars(cars.slice(0, 0 + portionSize));
+  }, [cars]);
+
+    return (
+      <div className="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th>Company</th>
+              <th>Model</th>
+              <th>VIN</th>
+              <th>Color</th>
+              <th>Year</th>
+              <th>Price</th>
+              <th>Availability</th>
+              <th>Actions</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-      <button className="load-more" onClick={loadMore}>
-        Load more
-      </button>
-    </div>
-  );
-}
+          </thead>
+          <tbody>
+            {displayCars?.map((car) => (
+              <tr key={car.id}>
+                <td>{car.car}</td>
+                <td>{car.car_model}</td>
+                <td>{car.car_vin}</td>
+                <td className="color-car">
+                  <div
+                    className="color-cirle"
+                    style={{ backgroundColor: `${car.car_color}` }}
+                  ></div>
+                  {car.car_color}
+                </td>
+                <td>{car.car_model_year}</td>
+                <td>{car.price}</td>
+                <td>{car.availability.toString()}</td>
+                <td>-Edit -Delete</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <button className="load-more" onClick={loadMore}>
+          Load more
+        </button>
+      </div>
+    );
+};
 
-export default Table
+export default Table;
